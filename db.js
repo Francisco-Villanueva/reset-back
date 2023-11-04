959a32d4577415390bc2d81a65f7f4978fb84a0a
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-04 12:25:31 -0300

    moving all files to root folder

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, RENDER_URL } = process.env;
 
 const db = new Sequelize(RENDER_URL, {
-  // host: DB_HOST,
   dialect: "postgres",
   logging: false,
 });
