22fba1823f32bd81b273100b9576d365911c32d4
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-06 09:20:59 -0300

    deleted db.sync

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const routes = require("./routes");
 const cors = require("cors");
 const db = require("./db");
 const models = require("./models");
+require("dotenv").config();
+
 const client = require("./repositories/whatsapper");
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:3000"],
 };
 
+const { PORT } = process.env;
 app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", routes);
 
-db.sync({ force: true }).then(() => {
-  app.listen(5432, () => {
-    console.log(`server running on port 4000, inicando wp`);
-  });
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}, inicando wp`);
 });
