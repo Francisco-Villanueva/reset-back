bc8a88e936a61d63b4cef568541da39295e4b1e4
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-06 12:34:44 -0300

    setting credentials to corsOptions

--- index.js
+++ index.js
@@ -10,16 +10,17 @@ const { PORT } = process.env;
 const client = require("./repositories/whatsapper");
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.use(cors());
+
 app.use(express.json());
 app.use("/api", routes);
-
 db.sync({ alter: true }).then(() => {
   console.log("db connected successfully");
 });
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}, inicando wp`);
 });
