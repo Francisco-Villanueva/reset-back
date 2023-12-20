c652bfb7bf8b1f002b55c954be3c7d503c88e7d0
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-12-20 16:56:59 -0300

    added api key

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const routes = require("./routes");
 const cors = require("cors");
 const db = require("./db");
 const models = require("./models");
+const path = require("path");
+
 require("dotenv").config();
 const client = require("./repositories/whatsapper");
 const corsOptions = {
@@ -20,9 +22,19 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
-app.use("/api", routes);
+// Middleware para verificar la API Key
+const verifyApiKey = (req, res, next) => {
+  const apiKey = req.query.API_KEY;
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ error: "Acceso no autorizado" });
+  }
+  next();
+};
+app.use(express.static(path.join(__dirname, "public")));
+// Aplicar el middleware a todas las rutas bajo /api
+// app.use("/api", routes);
+app.use("/api", verifyApiKey, routes);
 db.sync({ alter: true }).then(() => {
   console.log("db connected successfully");
 });
